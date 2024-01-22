18d6bf89a7d535ec3b5e77bbce88918daaba541a
shenrique1970 <shvribeiro@hotmail.com>
2024-01-22 11:23:20 -0300

    Novo desafio

---
+++ assets/js/js.js
@@ -0,0 +1,20 @@
+let circulo = document.querySelector(".circulo")
+let imgCopo = document.querySelector(".copo-grande>img")
+let verde = document.querySelector(".verde")
+let amarelo = document.querySelector(".amarelo")
+let rosa = document.querySelector(".rosa")
+
+amarelo.addEventListener("click", () => {
+    imgCopo.src = "assets/img/img2.png"
+    circulo.style.background = "#eb7495"
+})
+
+verde.addEventListener("click", () => {
+    imgCopo.src = "assets/img/img1.png"
+    circulo.style.background = "#017143"
+})
+
+rosa.addEventListener("click", () => {
+    imgCopo.src = "assets/img/img3.png"
+    circulo.style.background = "#d752b1"
+})
